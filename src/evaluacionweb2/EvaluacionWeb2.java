eb13676a8965ff23dbb383afd06d3a281e312734
Neider <jhonacevedocardenas@iesantaelena.edu.co>
2021-06-24 08:57:50 -0500

    punto 1

--- src/evaluacionweb2/EvaluacionWeb2.java
+++ src/evaluacionweb2/EvaluacionWeb2.java
@@ -1,19 +1,32 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package evaluacionweb2;
 
-/**
- *
- * @author NEIDER
- */
+import java.util.Scanner;
+
 public class EvaluacionWeb2 {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner leer=new Scanner(System.in);
+        
+        //PUNTO 1 --GALLINAS Y CONEJOS--
+        int gallinas,conejos,cabezas,patas,cab_pat,cabezas_y;
+        System.out.print("                          ---Bienvenido---                   ");
+        System.out.print("\n-Con este programa podrás calcular la cantidad de conejos y Gallinas que tienes en tu granja-");
+                
+        System.out.print("\n\n Ingrese la cantidad de Patas: ");
+        patas=leer.nextInt();
+        System.out.print("Ingre la cantidad de cabezas: ");
+        cabezas=leer.nextInt();
+        
+        cabezas_y=cabezas*-2;
+        cab_pat=cabezas_y+patas;
+       
+        conejos=cab_pat/2;
+        gallinas=cabezas-conejos;
+        
+        System.out.println("\n...RESULTADO...");
+        System.out.println("la cantidad de conejos son: "+conejos);
+        System.out.println("La cantidad de gallinas son: "+gallinas);
     }
 }
