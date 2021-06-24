a7bea5dae3ff715e8cff87a29e69fd0485ae8089
Neider <jhonacevedocardenas@iesantaelena.edu.co>
2021-06-24 10:47:13 -0500

    punto 2

--- src/evaluacionweb2/EvaluacionWeb2.java
+++ src/evaluacionweb2/EvaluacionWeb2.java
@@ -4,19 +4,19 @@ package evaluacionweb2;
 import java.util.Scanner;
 
 public class EvaluacionWeb2 {
-
     
     public static void main(String[] args) {
         Scanner leer=new Scanner(System.in);
         
         //PUNTO 1 --GALLINAS Y CONEJOS--
-        int gallinas,conejos,cabezas,patas,cab_pat,cabezas_y;
+        
+       /* int gallinas,conejos,cabezas,patas,cab_pat,cabezas_y;
         System.out.print("                          ---Bienvenido---                   ");
         System.out.print("\n-Con este programa podrás calcular la cantidad de conejos y Gallinas que tienes en tu granja-");
                 
-        System.out.print("\n\n Ingrese la cantidad de Patas: ");
+        System.out.print("\n\nIngrese la cantidad de Patas: ");
         patas=leer.nextInt();
-        System.out.print("Ingre la cantidad de cabezas: ");
+        System.out.print("Ingre la cantidad de Cabezas: ");
         cabezas=leer.nextInt();
         
         cabezas_y=cabezas*-2;
@@ -27,6 +27,34 @@ public class EvaluacionWeb2 {
         
         System.out.println("\n...RESULTADO...");
         System.out.println("la cantidad de conejos son: "+conejos);
-        System.out.println("La cantidad de gallinas son: "+gallinas);
+        System.out.println("La cantidad de gallinas son: "+gallinas);*/
+        
+        //PUNTO 2 --20 NUMEROS--
+        
+        /*int con_nega = 0;
+        System.out.print("                          ---Bienvenido---                   ");
+        System.out.print("\n-En este programa podras ingresar numeros positivos o negativos, y el mismo cuenta los numeros negativos-");
+        System.out.println("\n");
+        System.out.print("Digite la cantidad de numeros a almacenar: ");
+        int cantidadNumero=leer.nextInt();
+        int[]numeros=new int[cantidadNumero];
+       
+        for ( int i=0;i<cantidadNumero;i++){
+            System.out.print("Digite el numero: "+(i+1)+" : ");
+            int numero=leer.nextInt(); 
+            
+            if (numero<0){
+                con_nega=con_nega+1;
+            }
+        }
+        System.out.println("\n...RESULTADO...");
+        System.out.println("la cantidad de numeros Negativos Ingresados son: "+con_nega);*/
+        
+        
+        //PUNTO 3 -- SUMA DE COLUMNA --
+        
+      
+        
+        
     }
 }
